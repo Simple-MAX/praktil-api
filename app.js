c5dfed9a7a6979f5dc76cce919406f4d94629fc8
MartinAk <matin@lifeappliance.cc>
2019-04-06 21:24:28 +0200

    added compression

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
