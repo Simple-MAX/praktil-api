fe3bb0d1f2d6bd688c38319a5d9c9d7041eda188
MartinAk <matin@lifeappliance.cc>
2019-05-01 20:05:50 +0200

    added validator and session

--- app.js
+++ app.js
@@ -8,17 +8,26 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const compression = require('compression');
+const exphbs  = require('express-handlebars');
+const expressValidator = require('express-validator');
+const expressSession = require('express-session');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/', partialsDir: __dirname + '/views/partials/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
 app.use(logger('dev'));
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
 
 app.use('/', indexRouter);
 
